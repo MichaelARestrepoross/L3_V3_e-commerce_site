23a590ecc48354e8f010853ff719828390bf6376
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-05-07 22:36:13 -0400

    Updated ProdcutController with Crud functionality to product table

---
+++ server/ecommerce-site/src/main/java/com/ecommerce/controllers/ProductController.java
@@ -0,0 +1,49 @@
+package com.ecommerce.controllers;
+
+import com.ecommerce.models.Product;
+import com.ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setName(productDetails.getName());
+                    product.setDescription(productDetails.getDescription());
+                    product.setPrice(productDetails.getPrice());
+                    product.setImageUrl(productDetails.getImageUrl());
+                    return productRepository.save(product);
+                })
+                .orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
+    }
+}
