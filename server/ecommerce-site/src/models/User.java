d44872ef97e5bf67248db4cafb560183aece96ff
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-05-07 21:17:40 -0400

    Updated User.java i think it was wrong before. recreated it

--- server/ecommerce-site/src/models/User.java
+++ server/ecommerce-site/src/models/User.java
@@ -13,14 +13,21 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false)
-    private String password;
-
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
+    private String password;
 
-    
+    // Constructors
+    public User() {}
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     // Getters and Setters
     public Long getId() {
         return id;
@@ -38,19 +45,19 @@ public class User {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
