0ec7d650a7f3be083f44556532d862268d74b994
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-05-07 21:49:08 -0400

    Updated UserController to match current user table

--- server/ecommerce-site/src/main/java/com/ecommerce/controllers/UserController.java
+++ server/ecommerce-site/src/main/java/com/ecommerce/controllers/UserController.java
@@ -1,13 +1,47 @@
 package com.ecommerce.controllers;
 
+import com.ecommerce.models.User;
+import com.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @GetMapping("/test")
-    public String testEndpoint() {
-        return "User controller is working!";
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(updatedUser.getUsername());
+            user.setPassword(updatedUser.getPassword());
+            user.setEmail(updatedUser.getEmail());
+            return userRepository.save(user);
+        }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
     }
 }
