87d1b970de8eaf045a113a5361f08aa417cc95bc
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-05-14 23:10:04 -0400

    Updated Application to function with security

--- server/ecommerce-site/src/main/java/com/ecommerce/Application.java
+++ server/ecommerce-site/src/main/java/com/ecommerce/Application.java
@@ -2,11 +2,9 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
