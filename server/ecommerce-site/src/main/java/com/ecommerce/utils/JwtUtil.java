0530621e709374677de50eb4d7ee5fee6796ad6e
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-05-14 23:11:28 -0400

    Updated so security updates would run without crashing

---
+++ server/ecommerce-site/src/main/java/com/ecommerce/utils/JwtUtil.java
@@ -0,0 +1,6 @@
+package com.ecommerce.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
