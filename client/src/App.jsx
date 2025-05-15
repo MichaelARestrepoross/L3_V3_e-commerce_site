cbd65e171a6e23a1139118dd296b70b02a233cce
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2025-05-15 07:56:12 -0400

    Updated app with all routes and secured routes with login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import LoginForm from './pages/LoginForm';
+import Navbar from './components/Navbar';
+import LogoutButton from './components/LogoutButton';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  return (
-    <div>
-      <h1>E-commerce Site - Home Page</h1>
-      <Routes>
-        <Route path="/" element={<div>Welcome to the E-commerce Site</div>} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/login" element={<LoginForm />} />
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <div className="p-6">
+                                <h2>Welcome to the Home Page!</h2>
+                                <LogoutButton />
+                            </div>
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
